1a82bb48aae9d2e1cd7db57360395f3e04814fb5
assef <fernandoassef@hotmail.com>
2023-11-09 22:50:00 -0600

    build: crea get_tuples_list() y modifica definición de clean_date()

--- lib/functions.py
+++ lib/functions.py
@@ -10,7 +10,7 @@ del archivo principal 'export_data_wallet.py
 
 from selenium import webdriver as webdriver
 from selenium.webdriver.common.by import By
-from datetime import datetime
+from datetime import datetime, timedelta
 
 ACTUAL_YEAR = datetime.now().year
 
@@ -37,15 +37,31 @@ def get_dates(driver):
     return [date.text for date in driver.find_elements(By.CSS_SELECTOR, selector)]
 
 # ===============================================
-# Agrega el año en caso de ser el actual y
-# retorna la fecha en objeto datetime.
+# Retorna la fecha de string en objeto datetime.
 # ===============================================
 def clean_date(date_str: str):
     date_format = '%B %d, %Y'
     
-    # si los últimos 4 caracteres no son digitos,
-    # retorna agrega el año actual
-    if date_str[-4:].isdigit():
-        return datetime.strptime(date_str, date_format)
+    if date_str == 'Today':
+        return datetime.today().date()
+    elif date_str == 'Yesterday':
+        return (datetime.today() - timedelta(days = 1)).date()
+    elif not(date_str[-4:].isdigit()):
+        return datetime.strptime(date_str + f', {ACTUAL_YEAR}', date_format).date()
     else:
-        return datetime.strptime(date_str + f', {ACTUAL_YEAR}', date_format)
+        return datetime.strptime(date_str, date_format).date()
+
+# ===============================================
+# Retorna la lista de tuples donde cada elemento 
+# es un bloque de fecha. La estructura del tuple 
+# es (posición de la fecha 0 a n, cantidad de 
+# transacciones en esa fecha)
+# ===============================================
+def get_tuples_list(driver, dates):
+    data = []
+    xpath = '/html/body/div/div/div/main/div/div[2]/div[2]/div'
+    for i in range(len(dates)): # itera sobre las fechas
+        parent_div = driver.find_element(By.XPATH, f'{xpath}/div[{i+2}]') # divs de divisiones por fecha
+        child_divs = len(parent_div.find_elements(By.XPATH, './div')) - 1 # cantidad de divs adentro de cada bloque
+        data.append((i, child_divs)) # agrega a la lista el tuple (posición fecha, cantidad de elementos dentro)
+    return data
