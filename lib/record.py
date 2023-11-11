12b0ea90a60104a287b755d0e92731f971b4f28f
assef <fernandoassef@hotmail.com>
2023-11-10 23:31:03 -0600

    build: agrega el campo 'currency' a la definición de la clase Record

--- lib/record.py
+++ lib/record.py
@@ -12,11 +12,12 @@ cada registro como un objeto.
 # Definición clase Record para las transacciones.
 # ===============================================
 class Record:
-    def __init__(self, date, type, category, account, description, label, amount):
+    def __init__(self, date, type, category, account, description, label, currency, amount):
         self.date = date
         self.type = type
         self.category = category
         self.account = account
         self.description = description
         self.label = label
+        self.currency = currency
         self.amount = amount
